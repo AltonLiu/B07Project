5726b1f2033c3df3830884bfd74e6d9d272a997a
Alton Liu <alton.liu.cs@gmail.com>
2022-07-31 17:35:20 -0400

    Updated Activity class

--- app/src/main/java/com/example/b07project/Activity.java
+++ app/src/main/java/com/example/b07project/Activity.java
@@ -1,2 +1,11 @@
-package com.example.b07project;public class Activity {
+package com.example.b07project;
+
+public class Activity {
+    String name;
+    String numParticipants;
+
+    public Activity(String name, String numParticipants) {
+        this.name = name;
+        this.numParticipants = numParticipants;
+    }
 }
