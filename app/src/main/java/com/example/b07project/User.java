fefa99aa714f07342b4582cd97051a253ab3a4c8
Alton Liu <alton.liu.cs@gmail.com>
2022-07-31 18:36:06 -0400

    Added first and last name to User class

--- app/src/main/java/com/example/b07project/User.java
+++ app/src/main/java/com/example/b07project/User.java
@@ -2,11 +2,15 @@ package com.example.b07project;
 
 public abstract class User {
     String username;
+    String firstname;
+    String lastname;
     String password;
     String userType;
 
-    public User (String username, String password, String userType) {
+    public User (String username, String firstname, String lastname, String password, String userType) {
         this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
         this.password = password;
         this.userType = userType;
     }
