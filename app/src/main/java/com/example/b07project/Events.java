c89284d510fc2add9e8e22dc29e3e6db567b6a13
karlzi <karl.zi@hotmail.com>
2022-07-31 17:27:49 -0400

    Add activity to Event

--- app/src/main/java/com/example/b07project/Events.java
+++ app/src/main/java/com/example/b07project/Events.java
@@ -3,10 +3,12 @@ public class Event{
     long start_time[12];
     long end_time[12];
     Venue location;
-    public Event(int num, long start, long end, Venue location){
+    Activity sports_type;
+    public Event(int num, long start, long end, Venue location, Activity activity){
         this.players_num = num;
         this.start_time = start;
         this.end_time = end;
         this.location = loaction;
+        this.sports_type = activity;
     }
 }
