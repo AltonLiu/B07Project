0dbe6c16501c129f6ce17af1b3a43b2705386774
Alton Liu <alton.liu.cs@gmail.com>
2022-08-01 16:28:25 -0400

    Created Admin class

---
+++ app/src/main/java/com/example/b07project/backend/Admin.java
@@ -0,0 +1,26 @@
+package com.example.b07project.backend;
+
+import com.example.b07project.frontend.Activity;
+
+import java.util.*;
+
+public class Admin extends User {
+    public Admin() {
+        // Default constructor required for calls to DataSnapshot.getValue(Admin.class)
+        super();
+    }
+
+    public Admin (String username, String firstname, String lastname, String password) {
+        super(username, firstname, lastname, password, "Admin");
+    }
+
+    /**
+     * @param name Name of the venue
+     * @param address Address of the venue
+     * @param activities An array list of the activities available at the venue
+     */
+    public void createVenue (String name, String address, ArrayList<Activity> activities) {
+        Venue newVenue = new Venue(name, address, activities);
+        // Add code to upload newVenue to array list of all Venues on the server
+    }
+}
--- app/src/main/java/com/example/b07project/backend/User.java
+++ app/src/main/java/com/example/b07project/backend/User.java
@@ -7,7 +7,11 @@ public abstract class User {
     String password;
     String userType;
 
-    public User (String username, String firstname, String lastname, String password, String userType) {
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String username, String firstname, String lastname, String password, String userType) {
         this.username = username;
         this.firstname = firstname;
         this.lastname = lastname;
