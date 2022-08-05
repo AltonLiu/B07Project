cf3ac4d350f0dec71fe54bed6fd24dda4573352f
dopplerz <ryan_ramroop@outlook.com>
2022-08-04 22:40:46 -0400

    Created and Implemented RegisterActivity

---
+++ app/src/main/java/com/example/b07project/backend/UserFactory.java
@@ -0,0 +1,12 @@
+package com.example.b07project.backend;
+
+public class UserFactory {
+    public User getUser(String userType, String username, String firstname, String lastname, String email, String password){
+        if(userType == null){
+            return null;
+        }
+        if(userType.equals("Customer")){
+        }
+
+    }
+}
