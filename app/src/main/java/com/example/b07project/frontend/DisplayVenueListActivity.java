a5007b901038dd913fe7951b79dac3bd54cb5e11
Kevin Xiong <67486773+kevxiong@users.noreply.github.com>
2022-08-08 10:48:43 -0400

    Create DisplayVenusListActivity

---
+++ app/src/main/java/com/example/b07project/frontend/DisplayVenueListActivity.java
@@ -0,0 +1,21 @@
+package com.example.b07project.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.example.b07project.R;
+import com.example.b07project.backend.Customer;
+import com.example.b07project.backend.Event;
+
+import java.util.ArrayList;
+
+public class DisplayVenueListActivity {
+    ArrayList<Event> display_events;
+    
+}
